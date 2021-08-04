6a1e8e8e2014cef6f619fd124ed046a6b4581f53
Ian Mobley <imobley@taskrabbit.com>
2021-08-04 09:05:55 -0700

    Add aperture and set up scene

--- src/camera.rs
+++ src/camera.rs
@@ -6,10 +6,21 @@ pub struct Camera {
     lower_left_corner: Point3,
     horizontal: Vec3,
     vertical: Vec3,
+    cu: Vec3,
+    cv: Vec3,
+    lens_radius: f64,
 }
 
 impl Camera {
-    pub fn new(origin: Point3, focus: Point3, v_up: Vec3, v_fov: f64, aspect_ratio: f64) -> Camera {
+    pub fn new(
+        origin: Point3,
+        focus: Point3,
+        v_up: Vec3,
+        v_fov: f64,
+        aspect_ratio: f64,
+        aperture: f64,
+        focus_dist: f64,
+    ) -> Camera {
         // Vertical field-of-view in degrees
         let theta = std::f64::consts::PI / 180.0 * v_fov;
         let viewport_height = 2.0 * (theta / 2.0).tan();
@@ -19,23 +30,29 @@ impl Camera {
         let cu = v_up.cross(cw).normalized();
         let cv = cw.cross(cu);
 
-        let horizontal = viewport_width * cu;
-        let vertical = viewport_height * cv;
+        let horizontal = focus_dist * viewport_width * cu;
+        let vertical = focus_dist * viewport_height * cv;
 
-        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - cw;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - focus_dist * cw;
 
         Camera {
             origin,
             lower_left_corner,
             horizontal,
             vertical,
+            cu,
+            cv,
+            lens_radius: aperture / 2.0,
         }
     }
 
     pub fn get_ray(&self, s: f64, t: f64) -> Ray {
+        let rd = self.lens_radius * Vec3::random_in_unit_disk();
+        let offset = self.cu * rd.x() + self.cv * rd.y();
+
         Ray::new(
-            self.origin,
-            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
+            self.origin + offset,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset,
         )
     }
 }
--- src/vec.rs
+++ src/vec.rs
@@ -46,6 +46,18 @@ impl Vec3 {
         }
     }
 
+    pub fn random_in_unit_disk() -> Vec3 {
+        let mut rng = rand::thread_rng();
+
+        loop {
+            let p = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
+
+            if p.length() < 1.0 {
+                return p;
+            }
+        }
+    }
+
     pub fn x(self) -> f64 {
         self[0]
     }
